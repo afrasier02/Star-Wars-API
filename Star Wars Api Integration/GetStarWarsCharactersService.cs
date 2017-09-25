db15aa11913a67f68185e3c1d4f076a2d1065e57
Arron Frasier <arron.frasier@regions.com>
2017-09-25 11:25:19 -0500

    Add project files.

---
+++ Star Wars Api Integration/GetStarWarsCharactersService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Star_Wars_Api_Integration.Entities;
+using System.Net;
+
+namespace Star_Wars_Api_Integration
+{
+    public class GetStarWarsCharactersService
+    {
+        private readonly string _url;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="url"></param>
+        public GetStarWarsCharactersService(string url)
+        {
+            _url = url;
+        }
+
+        public StarWarsCharactersResponse GetStarWarsCharacters()
+        {
+            var jsonResponse = GetJsonResponse();
+
+            var response = JsonConvert.DeserializeObject<StarWarsCharactersResponse>(jsonResponse);
+
+            return response;
+        }
+
+        private string GetJsonResponse()
+        {
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString(_url);
+
+                return json;
+            }
+        }
+    }
+}
---
+++ Star Wars Api Integration/Tests/CharacterTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Wars_Api_Integration.Tests
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [TestFixture]
+    public class CharacterTests
+    {
+        private GetStarWarsCharactersService _service;
+        private string _url = "https://swapi.co/api/people";
+
+        /// <summary>
+        ///
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            _service = new GetStarWarsCharactersService(_url);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Test]
+        public void Service_InitialTest()
+        {
+            var response = _service.GetStarWarsCharacters();
+
+            Assert.Greater(Convert.ToInt32(response.count), 0);
+        }
+    }
+}
